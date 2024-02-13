96dc9fa141768f0de23b7b33286a08a0bd7c4788
LouisZA878 <dutoit183@gmail.com>
2024-02-13 14:59:36 +0000

    style - updated UI

--- client/app/page.js
+++ client/app/page.js
@@ -8,17 +8,13 @@ export default async function Home() {
 
   const user = await fetchUser().get('/api/auth/user')
   console.log(user)
-  console.log('h')
-  console.log('h')
-  console.log('h')
-  console.log('h')
   
 
 
 
   return (
     <div>
-        hey
+        no
     </div>
   )
 }
